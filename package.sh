65d7318c326fd00c312424d1dec8bc0c68cd02d7
Alexander Diebler <alexander.diebler@barzahlen.de>
2014-04-01 02:20:42 +0200

    Upload Package
    - created build script for upload to Shopware Community Store

---
+++ package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DST="barzahlen_shopware4_plugin_v1.0.4"
+if [ -d $DST ]; then
+rm -R $DST
+fi
+mkdir -p $DST/Frontend/ZerintPaymentBarzahlen
+cp -r src/engine/Shopware/Plugins/Local/Frontend/ZerintPaymentBarzahlen/ $DST/Frontend/ZerintPaymentBarzahlen/
+zip -r $DST.zip $DST/Frontend/
+rm -R $DST
