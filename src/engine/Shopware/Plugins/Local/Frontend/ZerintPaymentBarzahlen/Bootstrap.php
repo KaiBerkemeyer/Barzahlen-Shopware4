746a5b3e7dcc62caed8efdfd725eae1927375b42
Kai Berkemeyer <kai.berkemeyer@acid21.com>
2015-05-27 11:37:48 +0200

    Logo improvements

--- src/engine/Shopware/Plugins/Local/Frontend/ZerintPaymentBarzahlen/Bootstrap.php
+++ src/engine/Shopware/Plugins/Local/Frontend/ZerintPaymentBarzahlen/Bootstrap.php
@@ -356,7 +356,7 @@ class Shopware_Plugins_Frontend_ZerintPaymentBarzahlen_Bootstrap extends Shopwar
 
         for ($i = 1; $i <= 10; $i++) {
             $count = str_pad($i, 2, "0", STR_PAD_LEFT);
-            $description .= '<img src="https://cdn.barzahlen.de/images/barzahlen_partner_' . $count . '.png" alt="" style="vertical-align: middle; height: 25px;" />';
+            $description .= '<img src="https://cdn.barzahlen.de/images/barzahlen_partner_' . $count . '.png" alt="" style="height: 1em; vertical-align: -0.1em;" />';
         }
 
         $newData = array('additionaldescription' => $description);
